cb6cb691449914100f93e4412bf70c91f5b86058
Pro100x3mal <pro100x3mal@gmail.com>
2025-05-03 23:34:05 +0300

    first iteration

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -12,29 +15,84 @@ const (
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
 	// ваш код здесь
+	if size <= 0 {
+		return nil
+	}
+	genElements := make([]int, size)
+	src := rand.NewSource(time.Now().Unix())
+	for i := 0; i < size; i++ {
+		genElements[i] = rand.New(src).Int()
+	}
+	return genElements
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 || data == nil {
+		return 0
+	}
+	if len(data) == 1 {
+		return data[0]
+	}
+	maxNum := data[0]
+	for i := 1; i < len(data); i++ {
+		if data[i] > maxNum {
+			maxNum = data[i]
+		}
+	}
+	return maxNum
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	if len(data) < CHUNKS {
+		return maximum(data)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(CHUNKS)
+
+	maxNums := make([]int, CHUNKS)
+	chunkSize := len(data) / CHUNKS
+
+	for i := 0; i < CHUNKS; i++ {
+		start := i * chunkSize
+		end := start + chunkSize
+		if i == CHUNKS-1 {
+			end = len(data)
+		}
+
+		go func(i, start, end int) {
+			defer wg.Done()
+			maxNums[i] = maximum(data[start:end])
+		}(i, start, end) // добавил аргументы и параметры на случай, если версия go < 1.22
+	}
+	wg.Wait()
+	return maximum(maxNums)
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	genElements := generateRandomElements(SIZE)
+	if genElements == nil {
+		fmt.Println("SIZE must be greater than zero")
+		return
+	}
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
+	t := time.Now()
+	maxNum := maximum(genElements)
+	elapsed := time.Since(t).Microseconds()
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", maxNum, elapsed)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
-
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
 	// ваш код здесь
-
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	t = time.Now()
+	maxNum = maxChunks(genElements)
+	elapsed = time.Since(t).Microseconds()
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", maxNum, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,57 @@
 package main
 
-// Пишите тесты в этом файле
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateRandomElements(t *testing.T) {
+	testCases := []struct {
+		size    int
+		isNil   bool
+		isEmpty bool
+	}{
+		{size: -5, isNil: true},
+		{size: 0, isNil: true},
+		{size: 5, isNil: false, isEmpty: false},
+	}
+
+	for i, testCase := range testCases {
+		res := generateRandomElements(testCase.size)
+		if testCase.isNil {
+			assert.Nil(t, res, "test case: %d", i)
+		} else {
+			assert.NotNil(t, res, "test case: %d", i)
+			if !testCase.isEmpty {
+				assert.NotEmpty(t, res, "test case: %d", i)
+				assert.Len(t, res, testCase.size, "test case: %d", i)
+			}
+		}
+	}
+}
+
+func TestMaximum(t *testing.T) {
+	testCases := []struct {
+		data       []int
+		isEmpty    bool
+		maxElement int
+	}{
+		{data: nil, isEmpty: true},
+		{data: []int{}, isEmpty: true},
+		{data: []int{1}, isEmpty: false, maxElement: 1},
+		{data: []int{1, 2, 3, 4, 5}, isEmpty: false, maxElement: 5},
+		{data: []int{-1, -2, -3, -4, -5}, isEmpty: false, maxElement: -1},
+		{data: []int{-1, -2, -3, -4, -5, 0, 1, 2, 3, 4, 5}, isEmpty: false, maxElement: 5},
+	}
+	for i, testCase := range testCases {
+		res := maximum(testCase.data)
+
+		if testCase.isEmpty {
+			assert.Empty(t, res, "test case: %d", i)
+		} else {
+			assert.NotEmpty(t, res, "test case: %d", i)
+			assert.Equal(t, testCase.maxElement, res, "test case: %d", i)
+		}
+	}
+}
